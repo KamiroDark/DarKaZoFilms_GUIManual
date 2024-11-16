3092ab91a32811656e9f1a1a97440a87e3f9ba83
Camilo Prieto <prietocamilo21@gmail.com>
2024-11-16 14:39:40 -0500

    NEW >>>> Se agregan clases para la infraestructura del Cine

---
+++ src/co/edu/konradlorenz/model/Pelicula.java
@@ -0,0 +1,60 @@
+package co.edu.konradlorenz.model;
+
+public class Pelicula {
+	
+	//Atributos
+	private String titulo;
+	private String clasificacion;
+	private String genero;
+	private int duracion; // en minutos
+	
+	//Constructor
+	public Pelicula(String titulo, String clasificacion, String genero, int duracion) {
+		super();
+		this.titulo = titulo;
+		this.clasificacion = clasificacion;
+		this.genero = genero;
+		this.duracion = duracion;
+	}
+
+	public Pelicula() {
+	}
+	
+	//Carpinteria (Getters, Setters & toString)
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getClasificacion() {
+		return clasificacion;
+	}
+
+	public void setClasificacion(String clasificacion) {
+		this.clasificacion = clasificacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Pelicula [titulo=" + titulo + ", genero=" + genero + ", duracion=" + duracion + "]";
+	}
+}
---
+++ src/co/edu/konradlorenz/model/SillaGeneral.java
@@ -0,0 +1,10 @@
+package co.edu.konradlorenz.model;
+
+public interface SillaGeneral {
+	
+	//Variable(s)
+	static double PRECIOGENERAL = 9100;
+	
+	//Metodo(s)
+	public double calcularPrecioGeneral();
+}//Cierre Silla General
